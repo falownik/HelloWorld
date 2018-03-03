d20fe28a3053c9aaae579b3b5c6c2e93841df216
falownik <36796295+falownik@users.noreply.github.com>
2018-03-03 19:09:59 +0100

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#inlcude <cstdio>
+#include "HelloWorld"
+
+void HelloWorld::print(void){
+  cout << "Hello World!" << endl;
+  }
+  
+int main(void){
+  HelloWorld::HelloWolrd.print();
+  return 0;
+}
